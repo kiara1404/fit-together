a1dcc64469a9d69e9f716a84fb5df1d1ab5d48d6
Kiara <kiarabroekhuizen1@hotmail.com>
2020-06-20 10:12:19 +0200

    session added

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const port = 8000;
 const find = require('array-find');
 const bodyParser = require("body-parser"); 
-const mongo = require('mongodb')
+const mongo = require('mongodb');
+const session = require('express-session');
 
 require('dotenv').config();
 
 //database connect
-var db = null;
+let db = null;
 var url = 'mongodb+srv://' + process.env.DB_HOST;
 
 mongo.MongoClient.connect(url, 
@@ -30,28 +31,57 @@ app.set('view engine', 'ejs');
 app.set('views', 'view');
 //
 app.use(bodyParser.urlencoded({extended:true}))
+//session
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 30 , // duration before the session is lost = 30 min
+  }
+}))
+
+app.use(express.static('public'));
+app.use(notFound);
+
+app.post('/', add)
+app.post('/dashboard', login)
+app.post('/logout', logout)
+
 app.get('/', index);
 app.get('/people', people)
-app.use(express.static('public'));
-//app.use(notFound);
-app.post('/', add)
+app.get('/dashboard', dashboard)
 app.get('/add', form)
-app.get('/login', login)
+app.get('/login', loginForm)
 app.get('/:id', person)
+
 app.delete('/:id', remove)
 app.listen(port, function(){
     console.log('The  server is running')
 });
 
-
+//homepage without session
 function index(req, res) {
 res.render('index');
 }
 
-function login(req, res) {
+// only available when the user has a session, otherwise redirect to home
+function dashboard(req, res) {
+  if(req.session.user){
+  res.render('dashboard')
+  console.log(req.session)
+  }
+  else{
+    res.redirect('/')
+  }
+}
+
+// login page render
+function loginForm(req, res) {
   res.render('login');
   }
 
+  // list with all users
 function people(req, res){
   db.collection('users').find().toArray(done)
 
@@ -63,16 +93,16 @@ function people(req, res){
       }
     }
 }
-
+// not found redirect
 function notFound (req, res) {
-    res.status(404).redirect('/404.html')
+    res.status(404).redirect('/404')
   }
-
+// templating
 function form(req, res) {
    res.render('add');
 }
 
-
+// adding person to database
 function add(req,res,next){
     db.collection("users").insertOne({
     name: req.body.name,
@@ -88,14 +118,14 @@ function add(req,res,next){
         if(err) {
             next(err)
         } else {
-            res.redirect('/' + data.insertedId)
+            res.redirect('/' + data.insertedId) // route to new profile
             console.log('data input succes', req.body.name)
         }
     }
 }
 
 
-
+// detail page
 function person(req,res, next){
   let id = req.params.id;
   db.collection("users").findOne({
@@ -107,11 +137,12 @@ function person(req,res, next){
         next(err)
     } else {
         res.render('detail.ejs', {data:data});
-        console.log('person found succes')
+        console.log('person found succesfully')
+      }
     }
-}
-}
+  }
 
+// delete person from database
 function remove(req, res, next){
   var id = req.params.id
 
@@ -124,6 +155,41 @@ function remove(req, res, next){
       next(err)
     } else {
       res.json({status: 'ok'})
+          }
+      }
     }
+
+    // login with session
+function login(req, res, next) {
+  let person = req.body.name
+
+  db.collection("users").findOne({
+       profileId: person
+    },
+    done
+  );
+
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } 
+    else{
+    req.session.user = {user: data}
+      res.redirect('dashboard');
+    }
+  }
 }
+
+//kill session
+function logout(req, res, next) {
+  req.session.destroy(function (err) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  })
 }
+
+
+
